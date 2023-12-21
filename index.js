3b5e4260ee36d62e7d5a283732d486b90ea5a7ca
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2023-12-21 11:07:30 +0700

    export modules

---
+++ index.js
@@ -0,0 +1,16 @@
+const getAreaPath = './areas'
+
+module.exports = {
+    ////////////
+    // Areas //
+    //////////
+    circleGetArea: require(`${getAreaPath}`),
+    coneGetArea: require(`${getAreaPath}`),
+    rectangleGetArea: require(`${getAreaPath}`),
+    regularpolygonGetArea: require(`${getAreaPath}`),
+    rhombusGetArea: require(`${getAreaPath}`),
+    sphereGetArea: require(`${getAreaPath}`),
+    squareGetArea: require(`${getAreaPath}`),
+    trapezoidGetArea: require(`${getAreaPath}`),
+    triangleGetArea: require(`${getAreaPath}`)
+};
